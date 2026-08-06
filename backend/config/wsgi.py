8f7668f93ea9a606665499c4c5e61cada6d4fe83
Mir Minhaz Uddin <minhaz@example.com>
2026-08-07 00:02:03 +0600

    Add automatic WSGI application startup database migrator

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,5 +1,12 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 application = get_wsgi_application()
+
+try:
+    from django.core.management import call_command
+    call_command("migrate", interactive=False)
+except Exception as e:
+    print("WSGI auto-migrate error:", e)
